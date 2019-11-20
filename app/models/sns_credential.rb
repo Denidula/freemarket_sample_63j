dfe7c29f6f47a9d88d726a37d614bce65dde3165
meganezaru-kei <lostman0609@gmail.com>
2019-11-20 13:36:38 +0900

    add validations

--- app/models/sns_credential.rb
+++ app/models/sns_credential.rb
@@ -1,2 +1,5 @@
 class SnsCredential < ApplicationRecord
+  belongs_to :user
+  validates_presence_of :user_id, :uid, :provider # providerと:uidはもちろんのことユーザーとの紐付け(:user_id)も保証
+  validates_uniqueness_of :uid, uniqueness: {:scope => :provider} # providerと:uidのペアは一意であることを保証
 end
