ba9ee49a2a04645e72d32a757ca0f055cbd21421
Koidy <yuji.koido@gmail.com>
2019-11-20 10:29:10 +0900

    Update application_controller.rb
    basic認証を実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
+
+  private
+
+  def basic_auth
+    authenticate_or request_with_http_basic do |username, password|
+      username == 'admin' && password == '2222'
 end
