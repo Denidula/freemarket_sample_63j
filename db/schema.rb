a25d77bbce7b5c211c29350e7c3faacd628f0934
meganezaru-kei <lostman0609@gmail.com>
2019-11-21 12:21:39 +0900

    login function implementation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_20_042617) do
+ActiveRecord::Schema.define(version: 2019_11_20_043139) do
 
   create_table "sns_credentials", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "provider"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2019_11_20_042617) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["provider", "uid"], name: "index_sns_credentials_on_provider_and_uid", unique: true
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
