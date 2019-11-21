aec7a4031917e50ec61400eda888bcdd8a71b1d8
Koidy <yuji.koido@gmail.com>
2019-11-21 16:08:46 +0900

    Update routes.rb
    テスト用にtestsのコントローラとビューを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'tests#index'  # テスト用記述です。
   devise_for :users, :controllers => {
     :registrations => 'users/registrations',
     :sessions => 'users/sessions'   
