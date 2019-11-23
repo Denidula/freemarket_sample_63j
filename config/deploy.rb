67418034777fd6cca0b428cc440288b0eb985a0c
Koidy <yuji.koido@gmail.com>
2019-11-23 21:30:07 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,6 +8,8 @@ set :application, 'freemarket_sample_63j'
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:Denidula/freemarket_sample_63j.git'
 
+set :branch, 'deproy-function2' || "master"
+
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 set :linked_files, fetch(:linked_files, []).push("config/master.key")
