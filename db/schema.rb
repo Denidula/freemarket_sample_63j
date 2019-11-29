41dda710509d8e83c39ed1941c9217ce4067d360
meganezaru-kei <lostman0609@gmail.com>
2019-11-29 17:58:52 +0900

    add columns to credit_card
    credit_cardテーブルにカラムを追加します。

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_27_095939) do
+ActiveRecord::Schema.define(version: 2019_11_29_062849) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -52,6 +52,8 @@ ActiveRecord::Schema.define(version: 2019_11_27_095939) do
     t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "costomer_id", null: false
+    t.integer "card_id", null: false
   end
 
   create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
