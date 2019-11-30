e4ff7e5b5aa6125f983c95b7c643e2fc782f168a
meganezaru-kei <lostman0609@gmail.com>
2019-11-30 18:26:23 +0900

    routing settings
    ルーティングを見やすくなるように整理しました。（URIパターンも少し修正しています。）

--- config/routes.rb
+++ config/routes.rb
@@ -9,30 +9,24 @@ Rails.application.routes.draw do
 
   resources :signup, only: [:index,:create] do
     collection do
-      get 'input_user_info'
-      post 'input_user_info' => 'signup#input_user_info'
-      get 'input_phone_number'
+      get  'input_user_info'
+      post 'input_user_info'    => 'signup#input_user_info'
+      get  'input_phone_number'
       post 'input_phone_number' => 'signup#input_phone_number'
-      get 'input_address'
-      post 'input_address' => 'signup#input_address'
-      get 'input_credit_card'
-      post 'input_credit_card' => 'signup#input_credit_card'
-      get 'done'
-      post 'done' => 'signup#done'
+      get  'input_address'
+      post 'input_address'      => 'signup#input_address'
+      get  'input_credit_card'
+      post 'input_credit_card'  => 'signup#input_credit_card'
+      get  'done'
+      post 'done'               => 'signup#done'
     end
   end
   
-  namespace :logout do
-    get 'index'
-  end
-
-  namespace :mypage do
-    get 'index'
-    get 'notification'
-    get 'profile'
-    get 'card'
-    get 'identification'
-  end
+  get 'logout'                => 'logout#index'
+  get 'mypage'                => 'mypage#index'
+  get 'mypage/profile'        => 'mypage#profile'
+  get 'mypage/card'           => 'mypage#card'
+  get 'mypage/identification' => 'mypage#identification'
 
   resources :items do
     collection do
@@ -44,8 +38,8 @@ Rails.application.routes.draw do
 
   resources :credit_card, only: [:new, :show] do
     collection do
-      post 'show', to: 'credit_card#show'
-      post 'pay', to: 'credit_card#pay'
+      post 'show',   to: 'credit_card#show'
+      post 'pay',    to: 'credit_card#pay'
       post 'delete', to: 'credit_card#delete'
     end
   end
