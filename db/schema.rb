10197f47098bfa9f50132ea0645e267e9c68a131
Denidula <56705353+Denidula@users.noreply.github.com>
2019-12-03 19:15:57 +0900

    db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -34,22 +34,29 @@ ActiveRecord::Schema.define(version: 2019_12_02_111045) do
   end
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.integer "zip_code", null: false
     t.integer "prefecture_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "zip_code", null: false
     t.string "city", null: false
     t.string "address", null: false
     t.string "building"
     t.integer "user_id", null: false
+  end
+
+  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "ancestry"
   end
 
   create_table "credit_cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "card_id", null: false
-    t.string "customer_id", null: false
     t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "customer_id", null: false
+    t.string "card_id", null: false
   end
 
   create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -62,14 +69,16 @@ ActiveRecord::Schema.define(version: 2019_12_02_111045) do
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.integer "price", null: false
-    t.text "description", null: false
+    t.string "description", null: false
     t.string "size"
     t.string "status", null: false
     t.string "charge", null: false
     t.string "delivery_method", null: false
     t.string "send_date", null: false
-    t.integer "user_id", null: false
-    t.integer "category_id", null: false
+    t.integer "prefecture_id", null: false
+    t.string "parent_category"
+    t.string "child_category"
+    t.string "grandchild_category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -84,22 +93,22 @@ ActiveRecord::Schema.define(version: 2019_12_02_111045) do
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "nickname", null: false
     t.string "email", default: "", null: false
-    t.string "family_name_kanji", default: ""
-    t.string "first_name_kanji", default: ""
-    t.string "family_name_kana", default: ""
-    t.string "first_name_kana", default: ""
-    t.integer "birthday_year"
-    t.integer "birthday_month"
-    t.integer "birthday_day"
-    t.string "phone_number"
-    t.string "encrypted_password", null: false
+    t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "nickname", null: false
+    t.string "family_name_kanji", null: false
+    t.string "first_name_kanji", null: false
+    t.string "family_name_kana", null: false
+    t.string "first_name_kana", null: false
+    t.integer "birthday_year", null: false
+    t.integer "birthday_month", null: false
+    t.integer "birthday_day", null: false
+    t.string "phone_number"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
