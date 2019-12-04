ee9b48047a0b00b31e0aae99d5a530015a2b41a1
Denidula <56705353+Denidula@users.noreply.github.com>
2019-12-04 12:13:14 +0900

    + user_id
    Itemにuser_idを追加しました。

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_03_033217) do
+ActiveRecord::Schema.define(version: 2019_12_04_024950) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -51,13 +51,6 @@ ActiveRecord::Schema.define(version: 2019_12_03_033217) do
     t.string "ancestry"
   end
 
-  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "ancestry"
-  end
-
   create_table "credit_cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id", null: false
     t.datetime "created_at", null: false
@@ -79,9 +72,9 @@ ActiveRecord::Schema.define(version: 2019_12_03_033217) do
     t.string "parent_category"
     t.string "child_category"
     t.string "grandchild_category"
-    t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
   end
 
   create_table "sns_credentials", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
