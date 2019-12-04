09ae9e6c14f67dd4f3f7442a4f3c6842937b4fe2
Denidula <56705353+Denidula@users.noreply.github.com>
2019-12-04 19:44:44 +0900

    test (can save)
    保存できる場合のテストです。

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,15 +1,17 @@
 FactoryBot.define do
   factory :item do
     name { "MyString" }
-    price { 1 }
+    price { 300 }
     description { "MyString" }
     size { "MyString" }
     status { "MyString" }
     charge { "MyString" }
     delivery_method { "MyString" }
     send_date { "MyString" }
+    parent_category { "MyString" }
+    child_category { "MyString" }
+    grandchild_category { "MyString" }
     user_id { 1 }
-    category_id { 1 }
     created_at { Faker::Time.between(from: DateTime.now - 2, to: DateTime.now) }
   end
 end
