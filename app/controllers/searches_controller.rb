461afe7602c9560e19703504efdb40d54d51fb84
Denidula <56705353+Denidula@users.noreply.github.com>
2019-12-05 14:15:12 +0900

    display
    検索対象を一覧表示する

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,7 +1,7 @@
 class SearchesController < ApplicationController
 
   def index
-    @items = Item.search(params[:search]).limit(132)
+    @items = Item.search(params[:search]).order(created_at: :desc).limit(132)
     @search = params[:search]
   end
 
