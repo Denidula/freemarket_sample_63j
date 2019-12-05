c11d383bf30c8252bb4c92fa419877f6375744c7
Denidula <56705353+Denidula@users.noreply.github.com>
2019-12-05 12:21:37 +0900

    + search-function
    検索機能を追加（検索先ページはこのあと）

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,9 @@ class Item < ApplicationRecord
   belongs_to :user, optional: true
   has_many_attached :images
 
+  def self.search(search)
+    return Item.all unless search
+    Item.where(['name LIKE ?', "%#{search}%"])
+  end
+
 end
