0b1c9ee760a2698b85e66c11afcf88b7a34a521f
meganezaru-kei <lostman0609@gmail.com>
2019-12-16 15:53:42 +0700

    Update delete.js

--- app/assets/javascripts/delete.js
+++ app/assets/javascripts/delete.js
@@ -1,15 +1,11 @@
-document.addEventListener(
-  "turbolinks:load", e => {
-    let modal_open = document.getElementById("modal-open-btn");
-    modal_open.onclick = function () {
-      $('#overlay').fadeIn();
-      document.getElementById('modal-close-btn').onclick = function () {
-        $('#overlay').fadeOut();
-      };
-      document.getElementById("delete-comformation-btn").onclick = function () {
-        document.getElementById("item-delete-btn").click();
-      };
-    };
-  },
-  false
-);
+$(document).on("turbolinks:load", function(e){
+  var modal_open = $("#modal-open-btn").get(0);
+  var modal_close = $("#modal-close-btn").get(0);
+  var not_modalWindow = $("#modalWindow").not();
+  $(modal_open).on('click', function(){
+    $("#overlay").fadeIn();
+    $(modal_close).on('click', function(){
+      $("#overlay").fadeOut();
+    })
+  })
+});
